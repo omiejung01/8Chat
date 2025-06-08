000fbc0b30b3a13864b475b5d75772fc8fb980b5
Dhirachat Chayaporn <omiejung@gmail.com>
2025-06-08 17:23:08 +0700

    Back-End for Mes8 Chat - PHP - SQL and Streaming - Group-Card 04

--- chatapp/api/list_group.php
+++ chatapp/api/list_group.php
@@ -11,10 +11,12 @@ $stmt1 = $conn->prepare($sql1);
 $stmt1->bind_param("s", $email);
 
 $json_result = ["result" => "No data"];
+
+$group_id_list = array();
+
 if ($stmt1->execute()) {
 	$result1 = $stmt1->get_result();
 			
-	$group_id_list = array();
 			
 	while($row1 = $result1->fetch_assoc()) {
 		$group_id_list[] = $row1["group_id"];
@@ -29,5 +31,33 @@ if ($stmt1->execute()) {
 		}			
 	}*/
 }
+
+$sql2 = "SELECT user_email FROM participate WHERE group_id = ? AND void = 0 ORDER BY user_email ";
+		
+$group_count = count($group_id_list);
+		
+for ($i = 0 ; $i < $group_count; $i++) {
+			
+$stmt2 = $conn->prepare($sql2);
+$stmt2->bind_param("i",$group_id_list[$i]);
+
+$list_email = array();
+
+	if ($stmt2->execute()) {
+		$result2 = $stmt2->get_result();
+		
+		$email_list = array();
+								
+		while($row2 = $result2->fetch_assoc()) {
+			$email_list[] = $row2["user_email"];
+		}
+		$list_email[] = $email_list;
+		//$json_result = ["result" => "Success","group_list" => $group_id_list];
+
+	}
+	$json_result = ["result" => "Success","list_email" => $list_email];
+
+}
+
 echo json_encode($json_result);
 ?>
